23e2b0c299fd7e44724f83f2576d86e03cfe9560
Adam Zakaria <adam@zakaria.com>
2021-10-09 23:26:34 -0400

    Prototype of site seemingly working :)

---
+++ README.md
@@ -0,0 +1,5 @@
+To run:
+#window1
+npm run watch-sass
+#window2
+nodemon -e pug,js,md server.js
--- server.js
+++ server.js
@@ -5,67 +5,60 @@ const directory = './files/';
 const fs = require('fs');
 var path = require('path');
 const pug = require('pug');
+var favicon = require('serve-favicon');
+
+app.use(favicon(__dirname + '/public/images/favicon.ico')); 
 
 var showdown  = require('showdown'),
     converter = new showdown.Converter();
 
 let html;
+var postLinks = "";
+var postData = [];
+let markdownDir = '/Users/azakaria/Code/personal_projects/personal_site/markdown';
+let pageDir = '/Users/azakaria/Code/personal_projects/personal_site/pages';
 fs.readdir('/Users/azakaria/Code/personal_projects/personal_site/markdown', (err, files) => {
     files.forEach(file => {
+        let fileBaseName =file.split('.')[0];
+        let fileBaseNameNoUnderscores = fileBaseName.replace(/_/g, ' ');
+        postLinks += `<a href="/${fileBaseName}">${fileBaseNameNoUnderscores}</a><br>`; 
         fs.readFile(path.join('/Users/azakaria/Code/personal_projects/personal_site/markdown', file), 'utf8' , (err, data) => {
+            if (err) {
+                console.error(err)
+                return
+            }
+            html = converter.makeHtml(data);
+            const compiledFunction = pug.compileFile('templates/template.pug');
+            console.log(`postData: ${html}`);
+            let template = compiledFunction({ blog_post_text: html, blog_post_title: fileBaseNameNoUnderscores});
+            fs.writeFile(`/Users/azakaria/Code/personal_projects/personal_site/pages/${fileBaseName}.html`, template, err => {
+                if (err) {
+                    console.error(err)
+                    return
+                }
+                //file written successfully
+            })
+        })
+    });
+    const compiledFunctionHome = pug.compileFile('templates/homepage.pug');
+    let templateHome = compiledFunctionHome({ links: postLinks});
+    fs.writeFile(`/Users/azakaria/Code/personal_projects/personal_site/pages/homepage.html`, templateHome, err => {
         if (err) {
             console.error(err)
             return
         }
-            html = converter.makeHtml(data);
-
-            //https://pugjs.org/language/interpolation.html
-            let blogLink = `<a href="/post">${file}</a>`; //href = endpoint that triggers the reading of the markdown file? the sending of.
-
-            // Compile the source code
-            const compiledFunction = pug.compileFile('pug/template.pug');
-            const compiledFunctionHome = pug.compileFile('pug/homepage.pug');
-            let template = compiledFunction({ blog_post_text: data });
-            let templateHome = compiledFunctionHome({ links: blogLink });
-
-            fs.writeFile(`/Users/azakaria/Code/personal_projects/personal_site/templates/${file}.html`, template, err => {
-                if (err) {
-                    console.error(err)
-                    return
-                }
-            //file written successfully
-            })
-
-//@media (max-width: 830px){
-
-            fs.writeFile(`/Users/azakaria/Code/personal_projects/personal_site/templates/homepage.html`, templateHome, err => {
-                if (err) {
-                    console.error(err)
-                    return
-                }
-            //file written successfully
-            })
-
-
-            // Render a set of data
-            console.log();
-            // "<p>Timothy's Pug source code!</p>"
-            //console.log(html);
-
-
-        })
+        //file written successfully
     });
 });
 
 app.get('/', (req, res) => {
-  res.sendFile('/Users/azakaria/Code/personal_projects/personal_site/templates/homepage.html');
+  res.sendFile('/Users/azakaria/Code/personal_projects/personal_site/pages/homepage.html');
 })
 
-app.get('/post', (req, res) => {
-
-    res.sendFile('')
-    //I really want to load a template I think......
-    //...An html page with the blog post embedded in it..
+app.get('/:post', (req, res) => {
+    console.log(`post: ${path.join(markdownDir, req.params.post)}`)
+    console.log(`file: ${path.join(markdownDir, req.params.post, '.html')}`);
+    res.sendFile(path.join(pageDir, req.params.post) + '.html');
 })
 
 app.use("/public", express.static(__dirname + "/public"));
