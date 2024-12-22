2e5ce8557dde6bd1584dddc54e6c74ce2c5c5a13
Adam Zakaria <adam.p.zakaria@gmail.com>
2024-12-22 04:07:45 -0500

    Save

--- logFiles.js
+++ logFiles.js
@@ -3,28 +3,34 @@ import path from 'path';
 
 const directoryPath = path.join(process.cwd(), 'public/posts');
 
-fs.readdir(directoryPath, (err, files) => {
+fs.readdir(directoryPath, (err, fileNames) => {
   if (err) {
     return console.error('Unable to scan directory: ' + err);
   }
+  // make renderedPosts folder if it doesn't exist
+  if (!fs.existsSync('public/renderedPosts')) {
+    fs.mkdirSync('public/renderedPosts');
+  }
   console.log('Listing files in public/posts:');
-  files.forEach(file => {
+  fileNames.forEach(fileName => {
     /*
     For each file, create a new html file with the file name as the title
     in public/renderedPosts
     */ 
+    // get filename without extension
+    const baseName = fileName.split('.')[0];
     const html = `
     <!DOCTYPE html>
     <html>
       <head>
-        <title>${file}</title>
+        <title>${baseName}</title>
       </head>
       <body>
-        <h1>${file}</h1>
+        <h1>${baseName}</h1>
       </body>
     </html>
     `;
-    fs.writeFileSync(`public/renderedPosts/${file}.html`, html);
-    console.log(file);
+    fs.writeFileSync(`public/renderedPosts/${fileName}.html`, html);
+    console.log(fileName);
   });
 }); 
